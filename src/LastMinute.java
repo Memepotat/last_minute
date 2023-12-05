13edafeae1a0f312498d162477d09261d7784aab
P-NOULIS <148959003+P-NOULIS@users.noreply.github.com>
2023-12-05 19:07:34 +0200

    fixed issues in main

--- src/LastMinute.java
+++ src/LastMinute.java
@@ -9,8 +9,38 @@ import java.util.Comparator;
 public class LastMinute {
     public static void main(String[] args) throws URISyntaxException, IOException {
 
+        // * Check to see if recipes folder & recipesTEST exist, and if so deletes them
+        // before creating the new ones */
+        String path = System.getProperty("user.dir");
+
+        String recipePath = path + "/recipesTEST.txt";
+        File recipeTEST = new File(recipePath);
+        if (recipeTEST.delete()) {
+        } // *Delete recipeTEST file */
+
+        recipePath = path + "/recipesTEST1.txt";
+        File recipeTEST1 = new File(recipePath);
+        if (recipeTEST1.delete()) {
+        } // *Delete recipeTEST1 file */
+
+        path = path + "/recipes";
+        File file = new File(path);
+        if (file.exists()) {
+            // * Delete recipes directory */
+            try (var dirStream = Files.walk(Paths.get(path))) {
+                dirStream
+                        .map(Path::toFile)
+                        .sorted(Comparator.reverseOrder())
+                        .forEach(File::delete);
+            }
+        }
+
+        // * Test to see that the prompt is created correctly */
+        Ingredients ing = new Ingredients();
+        String Prompt = ing.PormptCreator(ing.IngredientList());
+        
         // * Call the api */
-        // GPT_API.getRecipes(Prompt);
+        GPT_API.getRecipes(Prompt);
         EditRecipe.subFile();
         EditRecipe.splitRecipes();
 
