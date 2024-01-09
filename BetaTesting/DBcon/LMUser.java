b8fd8a2cc925d8c9152b545b796ea399e0423eb4
Memepotat <ndamalos1967@gmail.com>
2024-01-09 19:53:40 +0200

    moved the login tests
    moved to betatesting since it's been incorporated
    
    Co-Authored-By: KostasNtg <147999020+KostasNtg@users.noreply.github.com>

---
+++ BetaTesting/DBcon/LMUser.java
@@ -0,0 +1,31 @@
+
+
+public class LMUser {
+
+    private String username;
+    private String password;
+
+
+    public LMUser(String username, String password) {
+        
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
